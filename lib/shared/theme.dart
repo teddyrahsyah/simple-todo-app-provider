4cf344e6c25e44f9900e02abae210e6fb48554b5
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-11-03 18:49:21 +0700

    setup theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color black = const Color(0xff272727);
+Color white = const Color(0xffFFFFFF);
+
+Color primary = const Color(0xffF76C6A);
+Color secondary = const Color(0xffF79E89);
+Color accent = const Color(0xffF0F0F0);
+
+TextStyle headlineTextStyle = GoogleFonts.bebasNeue(
+  color: primary,
+  fontSize: 36,
+);
+
+TextStyle titleTextTyle = GoogleFonts.montserrat(
+  color: white,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle contentTextStyle = GoogleFonts.montserrat(
+  color: white,
+);
+
+TextStyle contentSmallTextStyle = GoogleFonts.montserrat(
+  color: white,
+  fontSize: 11,
+);
+
+TextStyle labelTextStyle = GoogleFonts.montserrat(
+  color: white,
+  fontSize: 16,
+);
